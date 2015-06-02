96077d64eab1bc5263253afa6b5cbe7ad2feef01
Nicki Dlugash <nicki@mapbox.com>
2015-06-01 20:21:43 -0400

    some clean up

--- js/symbol/get_anchors.js
+++ js/symbol/get_anchors.js
@@ -20,43 +20,33 @@ function getAnchors(line, spacing, maxRepeatDistance, maxAngle, shapedText, shap
         shapedText ? shapedText.right - shapedText.left : 0,
         shapedIcon ? shapedIcon.right - shapedIcon.left : 0);
 
-// Would it be better to move this into addFeature function of symbol_bucket, since this
-// only needs to be set once per feature? But would also need to move labelLength.
-    var longLabelPadding = 50 * boxScale ; // should this be based on a property? 
-
+    var longLabelPadding = 100 * boxScale;
     if (spacing - labelLength * boxScale  < longLabelPadding) {
         //console.log("spacing: " + spacing);
         spacing = labelLength * boxScale + longLabelPadding;
         var adjustedSpacing = true;
         //console.log(adjustedSpacing);
-        //console.log("adjusted spacing: " + spacing + " boxScale: " + boxScale);
     }
 
-    // Offset the first anchor by half the label length, plus either the -repeat-distance length
+    // Offset the first anchor by half the label length, plus either the half the spacing
     // (if the line continued from outside the tile boundary), or a set extra offset.
 
     // Is the line continued from outside the tile boundary?
-    //if ((line[0].x == (0 || 4096)) || (line[0].y == (0 || 4096))) { 
-    // ^ why doesn't this work? 
-      if (line[0].x == 0 || line[0].x == 4096 || line[0].y == 0 || line[0].y == 4096) {  
+    if (line[0].x == 0 || line[0].x == 4096 || line[0].y == 0 || line[0].y == 4096) {  
         var continuedLine = true;
-        console.log("continuedLine: " + continuedLine);
+        //console.log("continuedLine: " + continuedLine);
     }
 
     // Add a bit of fixed extra offset for non-continued lines to avoid collisions at T intersections.
-    var extraOffset = glyphSize * 2;
+    var fixedExtraOffset = glyphSize * 2;
 
-    //var offset = (repeatDistance > 0 && continuedLine) ? 
-    // Maybe add another condition to use repeatDistance value first if there is one
-      var offset = !continuedLine ? 
-        ((labelLength / 2 + extraOffset) * boxScale * overscaling) % spacing :
+    var offset = !continuedLine ? 
+        ((labelLength / 2 + fixedExtraOffset) * boxScale * overscaling) % spacing :
         adjustedSpacing ?
         ((labelLength / 2 * boxScale + longLabelPadding) * overscaling) % spacing :
         ((labelLength / 2 * boxScale + spacing / 2) * overscaling) % spacing;
            
-        if(!continuedLine && !adjustedSpacing) { console.log(offset); }   
-
-    //if ((labelLength * boxScale) > spacing && continuedLine) { console.log((labelLength * boxScale) + " " + spacing + " " + offset); }
+    if(continuedLine && !adjustedSpacing) { console.log(offset); }   
 
     return resample(line, offset, spacing, angleWindowSize, maxAngle, labelLength * boxScale, continuedLine, false);
 }
